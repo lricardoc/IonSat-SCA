9497f9f3588acd0a4ab87c5c8f64a23ebb2424e5
shinok179 <127094666+shinok179@users.noreply.github.com>
2023-04-14 11:03:00 +0200

    Updating the all the Simulink blocks
    In the Simulink blocs :
    - /Disturbance Torques/Aerodynamic Model/Calculate - Az & El Angle : the bloc to compute the azimuth of the CubeSat have been corrected as it was incoherent
    - /Reference :
    a bloc was added into all the MODE blocs to "avoid" sign change of the quaternions
    
    In the Matlab scripts :
    -a code to compute the quaternions reference have been added
    
    SatConstant_Updated_04-2023. mat contains the updated satellite constants as of April of 2023

--- Simulink SDA/main_SDA.m
+++ Simulink SDA/main_SDA.m
@@ -34,6 +34,77 @@ MODE = 2;   %"sun-aero" pointing mode
 %where the state of the B-dot is enabled (1). 
 % 7: Reference quaternion is a custom quaternion that has to be defined by the 
 %user as an input to the mission block. 
+% 8: "sun-drag" Reference quaternion is such that z is aligned with 
+%the velocity direction to maximize the drag and x is aligned as best 
+%as possible with the velocity direction. 
+
+if MODE == 1
+    x_sa = orbit.v/norm(orbit.v);
+    z_sa = orbit.r/norm(orbit.r);
+    y_sa = -cross(x_sa,z_sa);
+    DCM_orb = horzcat(x_sa,y_sa,z_sa);
+    q_s2o = dcm2quat(DCM_orb);
+    att.q_i2r = (quatinv (q_s2o))';
+end
+
+if MODE == 2
+    x_sa = orbit.v/norm(orbit.v);
+    z_sa = orbit.sun_ECI_0 - dot(x_sa,orbit.sun_ECI_0)*x_sa;
+    z_sa = z_sa/norm(z_sa);
+    y_sa = cross(z_sa,x_sa);
+    DCM_sa = horzcat(x_sa,y_sa,z_sa);
+    q_s2i = dcm2quat(DCM_sa);
+    att.q_i2r = (quatinv(q_s2i))';
+end
+
+if MODE == 3
+    z_sa = orbit.sun_ECI_0/norm(orbit.sun_ECI_0);
+    x_sa = dot(orbit.v,z_sa)*z_sa - orbit.v;
+    x_sa = x_sa/norm(x_sa);
+    y_sa = cross(z_sa,x_sa);
+    DCM_sa=horzcat(x_sa,y_sa,z_sa);
+    q_s2i = dcm2quat(DCM_sa);
+    att.q_i2r = (quatinv (q_s2i))';
+end
+
+if MODE == 4
+    x_sa = orbit.r/norm(orbit.r);
+    z_sa = orbit.v/norm(orbit.v);
+    y_sa = cross(z_sa,x_sa);
+    DCM_sa=horzcat(x_sa,y_sa,z_sa);
+    q_s2i = dcm2quat(DCM_sa);
+    att.q_i2r = (quatinv (q_s2i))';
+end
+
+if MODE == 5
+    x_sa = - orbit.v/norm(orbit.v);
+    z_sa = orbit.sun_ECI_0 - dot(x_sa,orbit.sun_ECI_0)*x_sa;
+    z_sa = z_sa/norm(z_sa);
+    y_sa = cross(z_sa,x_sa);
+    DCM_sa = horzcat(x_sa,y_sa,z_sa);
+    q_s2i = dcm2quat(DCM_sa);
+    att.q_i2r = (quatinv(q_s2i))';
+end
+
+if MODE == 6
+    att.q_i2r = [1 0 0 0];
+end
+
+if MODE == 7
+    prompt = "What is the desired quaternion value? ";
+    att.q_i2r = input(prompt);
+end
+
+if MODE == 8
+    z_sa = orbit.sun_ECI_0/norm(orbit.sun_ECI_0);
+    nadir = - orbit.r/norm(orbit.r);
+    x_sa = dot(nadir,z_sa)*z_sa - nadir;
+    x_sa = x_sa/norm(x_sa);
+    y_sa = cross(z_sa,x_sa);
+    DCM_sa=horzcat(x_sa,y_sa,z_sa);
+    q_s2i = dcm2quat(DCM_sa);
+    att.q_i2r = (quatinv (q_s2i))';
+end
 
 %Initialisation of date
 date.year = 2024;
