48e37973e4a9015c25f01e125284f365da23582b
shinok179 <127094666+shinok179@users.noreply.github.com>
2023-06-02 14:08:49 +0200

    Full model update with the MEKF and UKF there is a script to run Monte Carlo simulations with teh full model. IonSatSimulationF.slx : Simulink model that has both PID and LQR controllers; also has both MEKF and UKF models

--- Simulink/main_ADCS_v2.m
+++ Simulink/main_ADCS_v2.m
@@ -151,7 +151,7 @@ att.wz0 = 0.01;
 %TimeStep = 0.25;        %fixed-step size in solver, Default time step=0.25
 TimeStep = 0.5;        %fixed-step size in solver, Default time step=0.25
 Torbit=2*pi*sqrt((orbit.a)^3/(3.986004418E5));
-N_orbits = 2;           %number of orbits to be simulated
+N_orbits = 1;           %number of orbits to be simulated
 %Time spent performing the simulation in seconds (one orbit is ~5400 s):
 t_sim = N_orbits*Torbit;
 %t_sim = 10800;
@@ -174,11 +174,97 @@ Enable_control = 0;
 %Enable whether the sensors are considering eclipses or not
 Enable_eclipse = 1;
 
-%Global time step
-global dt
+
+
+
+%Extended Kalman Filter
+global dt Tss Ts q_hat0 b_hat0 W6x6 Vmtm Vfss Vcss  
+%global PSDgyro PSDmtm PSDfss PSDcss PSDbias
+%Sensor parameters
 %Sampling times:
-Tss=1;         %sampling time for MTM, FSS, CSS could be 1 not 0.5
-Ts=1;          %sampling time for Gyro
+Tss=0.5;         %sampling time for MTM, FSS, CSS could be 1 not 0.5
+Ts=0.5;          %sampling time for Gyro
+%Sensor Noises
+%a) Magnetometer (MTM)
+sat.sensors.mag_sigma=5.0e-08;
+PSDmtm=sat.sensors.mag_sigma^2*Tss;
+%b) Gyrometer 
+sat.sensors.gyro_sigma=2.620e-04; %original value
+sat.sensors.gyro_sigma=0.2e-04; %to test
+sat.sensors.gyro_bias=1.160e-04; %original value
+sat.sensors.gyro_bias=0.160e-05; %to test
+PSDgyro=sat.sensors.gyro_sigma^2*Ts;
+PSDbia4s=sat.sensors.gyro_bias^2*Ts;
+b_offset=2e-4;
+b_offset=2e-2;
+b_offset=sat.sensors.gyro_bias;
+%b_offset=0e-2;
+b_offset2=-1e-2;    %for the triangular signal
+b_offset2=0;    %for the triangular signal
+%b_offset2=sat.sensors.gyro_bias;
+%c) Fin Sun Sensor (FSS)
+sat.sensors.sun_sigma=0.001164;
+PSDfss=sat.sensors.sun_sigma^2*Tss;
+%d) Coarse Sun Sensor (CSS)
+sat.sensors.sun_coarse_sigma=0.1745;
+PSDcss=sat.sensors.sun_coarse_sigma^2*Tss;
+
+
+%Other Kalman Filter Parameters:
+%initial values
+dt=Ts;
+q_hat0=[1;0;0;0]; 
+%q_hat0=[0.6;0.8;0.13;0.05]; q_hat0=q_hat0/norm(q_hat0); 
+q_hat0=[0.56 0.81 0.10 0.09]; q_hat0=q_hat0/norm(q_hat0); %ok
+% q_hat0=[0.56 0.81 0.05 0.09]; q_hat0=q_hat0/norm(q_hat0); %ok ok
+% q_hat0=[0.92 0.32 0.05 0.14]; q_hat0=q_hat0/norm(q_hat0); %very close 0
+b_hat0=[0;0;0];
+%b_hat0=[0.2e-3;0.2e-3;0.2e-3];
+
+
+%System state noise matrix, the covariance of the process noise matrix Wd
+Vg=sat.sensors.gyro_sigma^2;  %ok
+Vg=(5e-5)^2;
+Vb=sat.sensors.gyro_bias^2;   %ok
+Vb=(5.16e-6)^2; %OK
+Vb=(0.16e-5)^2; %OK
+%Vb=1e-12;
+
+Q11 = ((Vg*dt+(1/3)*Vb*dt^3))*eye(3);
+Q12 = (-1/2*Vb*dt^2)*eye(3);
+Q22 = (Vb*dt)*eye(3);
+Q = [ Q11 , Q12 ; Q12 , Q22];
+
+G=diag([-1 -1 -1 1 1 1]);
+W6x6 = G*Q*G';
+
+%for tests
+%A) FSS
+% Vfss=sat.sensors.sun_sigma^2;
+% Vfss=0.005^2; %repeat again here modify %ok 0.0012
+% Vfss=0.04^2; %repeat again here modify %ok 0.0012
+% Vfss=0.002^2; %repeat again here modify %ok 0.0012
+% V=Vfss*eye(3);
+
+%B) MTM and FSS
+% Vmtm=sat.sensors.mag_sigma^2; 
+% Vmtm=(2e-7)^2;
+% Vfss=sat.sensors.sun_sigma^2;
+% Vfss=0.002^2; %repeat again here modify %ok 0.0012
+% V=[Vmtm*eye(3),zeros(3);zeros(3),Vfss*eye(3)]; 
+
+%C) MTM, CSS and FSS
+Vmtm=sat.sensors.mag_sigma^2; 
+%Vmtm=(2e-7)^2;
+Vfss=sat.sensors.sun_sigma^2;
+%Vfss=0.002^2; %repeat again here modify %ok 0.0012
+Vcss=sat.sensors.sun_coarse_sigma^2;
+Vcss=0.01745^2;
+%Vcss=(1e-1)^2;
+
+
+
+
 
 %Define initial values for Kalman filter parameters
 x0 = [0.207740030841190 0.530411062089572 -0.675665322016929 0.467957858597196 1e-3 1e-3 1e-3];
@@ -188,7 +274,7 @@ What0 = [-8.56493764825016e-05 -0.000707789025389356 -0.000815029912348624];
 CovId = eye(6);
 
 dt = TimeStep;
-sigma_w = 1e-4;
+sigma_w = 1e-4;4
 sigma_b = 1e-5;
 sigma_s = 1e-3;
 sigma_m = 1e-6;
@@ -202,24 +288,12 @@ Q4 = ((sigma_b^2)*dt) .* eye(3);
 Qk = [Q1 Q2; Q3 Q4];
 GQG = Gk*Qk*(Gk.');
 
-%RWA
-% Calculate the pseudo inverse matrix
-Pinv_RW_repartition     %executes the following code lines:
-% alpha = 26.6 * pi/180;      %from deg to rad
-% sat.wheel.repartition_matrix_4RW = [[cos(alpha),      0,    -cos(alpha),      0    ];...
-%                                     [     0,     cos(alpha),     0,     -cos(alpha)];...
-%                                     [sin(alpha), sin(alpha), sin(alpha), sin(alpha)]];
-% sat.wheel.repartition_metrix_4RW_inverse = pinv(sat.wheel.repartition_matrix_4RW);
-
-%% Load other parameters
-% %load('SatConstants.mat')
-% %load('workspace.mat') %for 2021 simulation
-% sat.inertia = [0.06   0   0;...   %already saved
-%                 0   0.09  0;...
-%                 0     0   0.14];
-% sat.mass = 12;      %Satellite Mass [kg]
-% sat.CoG = [0;5.1;-4.2]/1000;   %Satellite Center of Gravity [m] in the BRF
-% %Needs to agree with the CoG calculated in the aerodynamic torque.
+%RWA Calculate the pseudo inverse matrix
+alpha = 26.6 * pi/180;      %from deg to rad
+sat.wheel.repartition_matrix_4RW = [[cos(alpha),      0,    -cos(alpha),      0    ];...
+                                    [     0,     cos(alpha),     0,     -cos(alpha)];...
+                                    [sin(alpha), sin(alpha), sin(alpha), sin(alpha)]];
+sat.wheel.repartition_metrix_4RW_inverse = pinv(sat.wheel.repartition_matrix_4RW);
 
 %% Other disturbance block parameters
 %%% Model of Perturbation Torques
@@ -259,11 +333,25 @@ sat.thruster.firstimpulse=4000;     %first thrust after start sim in [s]
                                     %needs to be less than waiting time
 sat.thruster.Nfirings=3;            %number of thrust firings
 
-%% Open the simulink model
-IonSatSimulationE
+%% Controller
+%LDR controller feedback gain
+K = [0.00053417  0  0  0.0070506  0  0;
+    0  0.00071733 0  0  0.00840481  0;
+    0  0  0.00081350 0  0  0.010476]; %q=0.004 r=4000
 
-%% Load the simulink model
-load_system("IonSatSimulationE.slx")
+%choose the controller to use : if PID then controller=1, 
+%if LQR then controller=0
+controller = 1;
 
-%% Run the simulink 
-simOut = sim("IonSatSimulationE");
+%choose the kalman filter : if UKF then  Kalman_filter = 1,
+%if MEKF then  Kalman_filter = 0,
+Kalman_filter = 0;
+
+% %% Open the simulink model
+% IonSatSimulationF
+% 
+% %% Load the simulink model
+% load_system("IonSatSimulationF.slx")
+% 
+% %% Run the simulink 
+% simOut = sim("IonSatSimulationF");
